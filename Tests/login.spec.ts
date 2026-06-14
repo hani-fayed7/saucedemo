44a3658bf316a5d9e7e49a497256256e1d67709b
Hani Fayed <hani.fayed@outlook.com>
2026-06-14 17:01:40 +0300

    Refactor login test structure to include setup and teardown steps

--- Tests/login.spec.ts
+++ Tests/login.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('SL-7: Verify user can login with valid credentials', async ({page}) => {
+test.beforeAll('Preconditions', async ({ page }) => {
+  console.log('Starting Login Tests');
+});
 
-  // Navigate to the login page
+test.beforeEach('Preconditions', async ({ page }) => {
+  console.log('Navigating To The Website');
+
+   // Navigate to the login page
   await page.goto('https://www.saucedemo.com/');
 
-  // Enter Valid Credentials
-  await page.locator("#user-name").fill('standard_user');
-  await page.locator("#password").fill('secret_sauce');
+});
 
-  // Click login button
-  await page.locator("#login-button").click();
+async function successfulLogin(page: any) {
+  // Expected Results for successful login
 
-  // Expected Results
-  
   // Verify that the user is redirected to the inventory page
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
   await expect(page).toHaveTitle(/Swag Labs/);
@@ -26,5 +27,22 @@ test('SL-7: Verify user can login with valid credentials', async ({page}) => {
 
   // Verify that the sort dropdown is visible
   await expect(page.locator(".product_sort_container")).toBeVisible();
+}
 
+test('SL-7: Verify user can login with valid credentials', async ({page}) => {
+
+  // Enter Valid Credentials
+  await page.locator("#user-name").fill('standard_user');
+  await page.locator("#password").fill('secret_sauce');
+
+  // Click login button 
+  await page.locator("#login-button").click(); 
+
+  // Expected Results for successful login
+  await successfulLogin(page);
+});
+
+test.afterAll('Expected Results', async ({ page }) => {
+  console.log('Login Tests Completed');
+  await page.close();
 });
